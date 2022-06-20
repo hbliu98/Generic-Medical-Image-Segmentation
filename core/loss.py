cf11fc811092a3285922ecb7edcdbd464026819b
hbliu98 <hbliu98@gmail.com>
2022-06-20 17:42:50 +0800

    fix loss.py

--- core/loss.py
+++ core/loss.py
@@ -6,15 +6,17 @@ class DiceLoss(nn.Module):
     def __init__(self, ignore_bg=False) -> None:
         super().__init__()
         self.nonline = nn.Softmax(dim=1)
-        self.smooth = 1.
+        self.smooth = 1e-5
         self.ignore_bg = ignore_bg
     
     def forward(self, x, y):
         with torch.no_grad():
             y_onehot = torch.zeros_like(x)
-            y_onehot.scatter(1, y.long(), 1)
+            y_onehot = y_onehot.scatter(1, y.long(), 1)
         axes = [0] + list(range(2, len(x.shape)))
         
+        x = self.nonline(x)
+
         tp = (x * y_onehot).sum(axes)
         fp = (x * (1 - y_onehot)).sum(axes)
         fn = ((1 - x) * y_onehot).sum(axes)
